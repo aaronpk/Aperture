3a09001567b3d783ee7d07cc1c810c0c7eeee2e7
Aaron Parecki <aaron@parecki.com>
2018-04-20 10:03:54 -0700

    add signup form

--- aperture/resources/views/welcome.blade.php
+++ aperture/resources/views/welcome.blade.php
@@ -18,12 +18,11 @@
                 color: #636b6f;
                 font-family: 'Lato', sans-serif;
                 font-weight: 100;
-                height: 100vh;
                 margin: 0;
             }
 
             .full-height {
-                height: 100vh;
+                height: 95vh;
             }
 
             .flex-center {
@@ -64,21 +63,56 @@
                 margin-bottom: 30px;
             }
 
-            .blog {
-                max-width: 500px;
-                margin: 0 auto;
-                padding-bottom: 100px;
+
+            .signup {
+              background: #eee;
             }
 
-            .blog .published {
-                font-size: 0.7em;
-                padding: 10px 0;
+
+            .credits {
+              padding: 3em;
             }
 
-            .blog img {
-                margin: 0 auto;
-                width: 360px;
-                display: block;
+
+            /* Mailchimp Form */
+            #mc_embed_signup {
+              width: 260px;
+              text-align: center;
+            }
+
+            .mc-field-group {
+              margin-bottom: 0.5em;
+            }
+
+            .mc-field-group label {
+              display: block;
+            }
+
+            #mc_embed_signup input {
+              display: block;
+              background: #fff;
+              color: black;
+              border: 2px #444 solid;
+              border-radius: 4px;
+              padding: 1em 2em;
+              font-size: 1em;
+              margin-left: auto;
+              margin-right: auto;
+            }
+
+            #mc_embed_signup .button {
+              border: 2px #444 solid;
+              border-radius: 4px;
+              cursor: pointer;
+              background: #ddd;
+              color: black;
+              text-decoration: none;
+              margin-top: 1em;
+              margin-left: auto;
+              margin-right: auto;
+            }
+            #mc_embed_signup .button:hover {
+              background: #ccc;
             }
         </style>
     </head>
@@ -102,5 +136,41 @@
                 <a href="/" class="u-url"></a>
             </div>
         </div>
+
+        @if(env('EMAIL_SIGNUP'))
+        <div class="flex-center position-ref full-height signup">
+          <div class="content">
+
+            <!-- Begin MailChimp Signup Form -->
+            <div id="mc_embed_signup">
+              <form action="https://nicernet.us12.list-manage.com/subscribe/post?u=3da16cdb35a3696d18f3d5001&amp;id=22aff5733e" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                <div id="mc_embed_signup_scroll">
+                  <div class="mc-field-group full-width">
+                    <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" placeholder="enter your email">
+                  </div>
+                  <div id="mce-responses" class="clear">
+                    <div class="response" id="mce-error-response" style="display:none"></div>
+                    <div class="response" id="mce-success-response" style="display:none"></div>
+                  </div>
+                  <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_3da16cdb35a3696d18f3d5001_22aff5733e" tabindex="-1" value=""></div>
+                  <div class="clear full-width">
+                    <input type="submit" value="Keep me updated!" name="subscribe" id="mc-embedded-subscribe" class="button">
+                  </div>
+                </div>
+                <input type="hidden" name="SOURCE" value="monocle">
+              </form>
+            </div>
+            <!--End mc_embed_signup-->
+
+          </div>
+        </div>
+        @endif
+
+
+        <div class="flex-center position-ref credits">
+          <div>Aperture is created by <a href="https://aaronparecki.com/">Aaron Parecki</a> and is part of the <a href="https://indieweb.org/">IndieWeb</a>.</div>
+        </div>
+
+
     </body>
 </html>
