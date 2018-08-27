35949247fcc228fb07a1a1daa04e0af9aa92f701
Aaron Parecki <aaron@parecki.com>
2018-08-27 07:58:26 -0700

    fix up manifest

--- aperture/resources/views/components/favicon.blade.php
+++ aperture/resources/views/components/favicon.blade.php
@@ -1,7 +1,7 @@
         <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png">
         <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png">
         <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png">
-        <link rel="manifest" href="/icons/site.webmanifest">
+        <link rel="manifest" href="/icons/manifest.json">
         <link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#00b1bf">
         <link rel="shortcut icon" href="/icons/favicon.ico">
         <meta name="msapplication-TileColor" content="#004a50">
