01f87e36ea8190024f080c22bb0a93e4619feb23
Aaron Parecki <aaron@parecki.com>
2018-11-06 14:27:45 +0100

    return more detailed error messages around invalid tokens

--- aperture/app/Http/Middleware/VerifyIndieAuthAccessToken.php
+++ aperture/app/Http/Middleware/VerifyIndieAuthAccessToken.php
@@ -24,14 +24,26 @@ class VerifyIndieAuthAccessToken
         // Check if this user exists
         $user = User::where('id', $match[1])->first();
         if(!$user) {
-            return Response::json(['error'=>'not_found'], 404);
+            return Response::json([
+              'error' => 'not_found',
+              'error_description' => 'This user ID does not exist'
+            ], 404);
         }
 
         // Check the given access token at the token endpoint
         $authorization = $request->header('Authorization');
         if($authorization) {
             if(!preg_match('/Bearer (.+)/', $authorization, $match)) {
-                return Response::json(['error'=>'unauthorized'], 401);
+                // Check for a plain token and reject with a specific error
+                if(preg_match('/^[^ ]+$/', $authorization)) {
+                    $error_description = 'The Authorization header did not contain a Bearer token';
+                } else {
+                    $error_description = 'The Authorization header was invalid';
+                }
+                return Response::json([
+                  'error' => 'unauthorized',
+                  'error_description' => $error_description
+                ], 401);
             }
             $token = $match[1];
         } else {
@@ -39,7 +51,10 @@ class VerifyIndieAuthAccessToken
         }
 
         if(!$token) {
-            return Response::json(['error'=>'unauthorized'], 401);
+            return Response::json([
+              'error' => 'unauthorized',
+              'error_description' => 'There was no access token in the request'
+            ], 401);
         }
 
         // Check the cache
@@ -71,15 +86,19 @@ class VerifyIndieAuthAccessToken
                     'Accept: application/json'
                 ]);
 
-                // The token endpoint returns 200 for a valid token
+                // The token endpoint should return 200 for a valid token
                 if($token_response['code'] != 200) {
+                    $body = $token_response['body'];
+                    $decoded = @json_decode($body);
+                    if($decoded) $body = $decoded;
+
                     return Response::json([
                         'error' => 'forbidden',
                         'error_description' => 'The token endpoint could not verify this access token',
                         'token_endpoint' => [
                             'url' => $user->token_endpoint,
                             'code' => $token_response['code'],
-                            'response' => $token_response['body'],
+                            'response' => $body,
                         ]
                     ], 403);
                 }
@@ -93,12 +112,29 @@ class VerifyIndieAuthAccessToken
                   parse_str($token_response['body'], $token_data);
                 }
 
-                if(!$token_data || !isset($token_data['me'])) {
-                    return Response::json(['error'=>'invalid_token_response'], 400);
+                if(!$token_data) {
+                    return Response::json([
+                      'error' => 'invalid_token_response',
+                      'error_description' => 'The token endpoint did not return valid token data'
+                    ], 400);
+                }
+
+                if(!isset($token_data['me'])) {
+                    return Response::json([
+                      'error' => 'invalid_token_response',
+                      'error_description' => 'The token endpoint did not return a "me" URL for the token'
+                    ], 400);
                 }
 
                 if(\IndieAuth\Client::normalizeMeURL($token_data['me']) != \IndieAuth\Client::normalizeMeURL($user->url)) {
-                    return Response::json(['error'=>'invalid_user'], 403);
+                    return Response::json([
+                      'error' => 'invalid_user',
+                      'error_description' => 'This token was issued to a different user',
+                      'debug' => [
+                        'expected' => \IndieAuth\Client::normalizeMeURL($user->url),
+                        'from_token' => $token_data['me'],
+                      ]
+                    ], 403);
                 }
 
                 $token_data['type'] = 'indieauth';
